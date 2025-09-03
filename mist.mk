ae7d24842f6a2cb1a254a972d8b0e036eec2e285
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-03 09:11:40 +0800

    extras: add partial screenshot
    Change-Id: I37e1e77ba3d7913e91e8c88b730319f85f269417

--- mist.mk
+++ mist.mk
@@ -384,6 +384,10 @@ PRODUCT_PACKAGES += \
 # Game Props
 TARGET_PRODUCT_PROP += vendor/extras/gameprops/gameprops.prop
 
+# Partial screenshot
+PRODUCT_PACKAGES += \
+    ScreenshotEditor
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
